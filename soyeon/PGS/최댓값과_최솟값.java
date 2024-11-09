59bc840cf997e3afdcbca12a42717d8aab18db7a
soyeon <ksy1118s@naver.com>
2024-11-09 17:04:22 +0900

    refactor: 최솟값과 최댓값 계산 로직 간소화 및 불필요한 배열 제거

--- soyeon/PGS/최댓값과_최솟값.java
+++ soyeon/PGS/최댓값과_최솟값.java
@@ -1,28 +1,16 @@
 public class 최댓값과_최솟값 {
     public String solution(String s) {
-        String answer = "";
+        String[] numbers = s.split(" ");
         
-        String[] num = s.split(" ");
-
-        int[] number = new int[num.length];
-        for(int i = 0; i < num.length; i++){
-            number[i] = Integer.parseInt(num[i]);
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (String numStr : numbers) {
+            int num = Integer.parseInt(numStr);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
         }
         
-        int min = number[0];
-        int max = number[0];
-        
-        for(int i = 1; i < number.length; i++){
-            if(number[i] < min){
-                min = number[i];
-            }
-            if(number[i] > max){
-                max = number[i];
-            }
-        }
-        
-        answer = Integer.toString(min) + " " + Integer.toString(max);
-        
-        return answer;
+        return min + " " + max;
     }
 }
